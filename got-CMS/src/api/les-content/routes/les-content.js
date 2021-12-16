7289cb8da983a96e21cc59a5a192f9f03140e3f6
beaupd <beau@patrickdekker.nl>
2021-12-16 13:36:35 +0100

    remove api from main, switched api to api branch

--- got-CMS/src/api/les-content/routes/les-content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * les-content router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::les-content.les-content');
--- got-CMS/src/api/volume/controllers/volume.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  volume controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::volume.volume');
